24797cd3b2041b3553af28739c4556f5232ad03e
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-10 20:09:16 +0530

    main nav bar

--- src/Components/header.js
+++ src/Components/header.js
@@ -1,2 +1,18 @@
 import React from 'react';
 
+function Header(){
+<ul class="nav justify-content-end">
+<li class="nav-item">
+  <a class="nav-link active" aria-current="page" href="#">Active</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Link</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link" href="#">Link</a>
+</li>
+<li class="nav-item">
+  <a class="nav-link disabled">Disabled</a>
+</li>
+</ul>
+}
