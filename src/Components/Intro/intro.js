4b64df6a060dd26ab81be19bb90559a3f8a20368
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-12 17:38:34 +0530

    about added

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -6,12 +6,14 @@ const Intro= () => {
     return (
         <div className="main-box">
         <div className="main-box-content-left">
-            <span className="user-id">Pranjal Singh</span>
+            <span className="first-name">Pranjal</span>
+            <span className="last-name">Singh </span>
+            <div className="about-user">Hello World! ldfvedskjdsnpranjl singh jdfbunjdvcs ipranjla pranjal singh prabnjsbdasn bjbdfusujdfbnieeurehgbvjs </div>
         </div>
         <div className="main-box-content-right">
             <img className="profile-picture" src={logo} alt="hello"/>
         </div>
-       
+        
         </div>
     )
 }
