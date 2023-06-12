5012f036234fa808a8b13ccbd5819e54727dae1c
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-12 07:23:38 +0530

    main component started

---
+++ src/Components/Intro/intro.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './intro.css';
+
+const Intro= () => {
+    return (
+        <div className="main-box">
+        <div className="main-box-content-left">
+            <span className="user-id">Pranjal Singh</span>
+        </div>
+        
+        <div className="main-box-content-right">
+            <img src="public\logo192.png" alt="hello"></img>
+        </div>
+       
+        </div>
+    )
+}
+
+export default Intro;
+
+
