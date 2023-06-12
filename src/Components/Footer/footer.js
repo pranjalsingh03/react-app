5a4b6d02ac0fa78e634e10119ae903498d84be92
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-12 22:28:43 +0530

    footer updated backgriiund color

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,12 +1,13 @@
 import React from "react";
+import "./footer.css";
 
 const Footer = () => {
     return (
         <div>
         <footer className="bg-light text-center text-lg-start">
-            <div className="text-center p-3" style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}>
+            <div className="text-center p-3" >
             © 2021 Pranjal Singh:
-            <a className="text-dark" href="#">
+            <a className="text-dark" href="www.google.com">
             </a>
             </div>
         </footer>
