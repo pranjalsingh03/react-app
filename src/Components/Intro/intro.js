f23eadc2ce9f75f0ababd30a52750edb4f2ec3f1
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-12 17:40:41 +0530

    commenting part

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -4,12 +4,15 @@ import logo from "../../Images/profile.JPG";
 
 const Intro= () => {
     return (
+        //Main box of introdunction page
         <div className="main-box">
+            {/* left side of the div */}
         <div className="main-box-content-left">
             <span className="first-name">Pranjal</span>
             <span className="last-name">Singh </span>
             <div className="about-user">Hello World! ldfvedskjdsnpranjl singh jdfbunjdvcs ipranjla pranjal singh prabnjsbdasn bjbdfusujdfbnieeurehgbvjs </div>
         </div>
+        {/* Right side of the div */}
         <div className="main-box-content-right">
             <img className="profile-picture" src={logo} alt="hello"/>
         </div>
