d5e3eb0fcad022784f383c8a370e9ae9f59ad264
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-13 14:06:34 +0530

    left side container added

--- src/Components/Home/home.js
+++ src/Components/Home/home.js
@@ -1,9 +1,29 @@
 import React from "react";
+import "./home.css"
 
-const Home =() =>{
-    return(
-        <div>
-            <h1>Home</h1>
+const Home = () => {
+    return (
+        <div className="main-box-area">
+            <div className="main-box-area-left">
+                <div className="main-box-area-left-content">
+                    <div className="main-box-area-left-content-heading">
+                        <span
+                            className="main-box-area-left-content-heading-text">
+                            About Me</span>
+                    </div>
+                    <div className="main-box-area-left-content-para">
+                        <span className="main-box-area-left-content-para-text">
+                            Hello World! ldfvedskjdsnpranjl singh jdfbunjdvcs
+                            ipranjla pranjal singh prabnjsbdasn
+                            bjbdfusujdfbnieeurehgbvjs
+                        </span>
+                    </div>
+                </div>
+                <div className="main-box-area-right">
+                    <div className="main-box-area-right-content">
+                        </div> 
+                </div>
+            </div>
         </div>
     )
 }
