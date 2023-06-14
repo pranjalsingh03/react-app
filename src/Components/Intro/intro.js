a39532dcc1dcc9ffb652e34247c4e8828ecc8fba
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-14 21:50:19 +0530

    update

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -1,6 +1,5 @@
 import React from "react";
 import './intro.css';
-import logo from "../../Images/profile.JPG";
 import github from "../../Images/github.svg";
 import linkedin from "../../Images/linkedin.svg";
 import instagram from "../../Images/instagram.svg";
@@ -14,8 +13,7 @@ const Intro = () => {
         
         {/* left side of the div */}
             <div className="main-box-content-left">
-                <span className="first-name">Pranjal</span>
-                <span className="last-name">Singh </span>
+                <span className="first-name">Pranjal Singh</span>
                 <div className="about-user">Hello World! ldfvedskjdsnpranjl singh jdfbunjdvcs ipranjla pranjal singh prabnjsbdasn bjbdfusujdfbnieeurehgbvjs
                 </div>
                 <div orientation="left"
@@ -30,12 +28,9 @@ const Intro = () => {
                 </div>
             </div>
             {/* Right side of the div */}
-            <div className="main-box-content-right">
-                <img className="profile-picture" src={logo} alt="hello" />
+                
                 <div orientation="right" class="side__StyledContainer-sc-18y99l7-0 lbtSgG"><div class="email__StyledLinkWrapper-sc-1fu3fru-0 dvseSq fade-enter-done"><a href="mailto:pranjalmagansingh9304@gmail.com" class="email__StyledEmailLink-sc-1fu3fru-1 gduzAh">pranjalmagansingh9304@gmail.com</a></div></div>
             </div>
-
-        </div>
     )
 }
 
