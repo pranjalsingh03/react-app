b39da7e426e8883382d275b734dade81676f89dd
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-18 22:04:20 +0530

    imple

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Header from "./Components/Header/header"
 import Footer from "./Components/Footer/footer"
 import Intro from './Components/Intro/intro';
 import Home from "./Components/Home/home";
+import Projects from './Components/Projects/projects';
 
 
 export default function MyApp() {
@@ -12,6 +13,7 @@ export default function MyApp() {
       <Header />    {/*Header of the portfolio*/}
       <Intro/>       {/* introduction  */}
       <Home/>       {/*Home page */}
+      <Projects/>
       <Footer/>     {/*//Footer of the portfolio */}
        
     </div>
