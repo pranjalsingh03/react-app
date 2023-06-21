ed4fe590ec4e7ed63c0674982a52d18ae5c92b8f
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-21 12:51:27 +0530

    added projects

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -4,11 +4,56 @@ import "./project.css";
 const Projects = () => {
 
     return (
-        
+
         <div className="main-content-area-box">
-         <h2 className="headline">Projects</h2> 
-        </div>
-    );
+            <h2 className="headline">Projects</h2>
+            <div className="project-box">
+                <div className="project">
+                    <h3 className="project-title">Project 1</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                    https://github.com/josephwinston/portfolio" target="_blank"><button
+                            className="project-button">View Code</button></a>
+                </div>
+                <div className="project">
+                    <h3 className="project-title">Project 2</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                    https://github.com/josephwinston/weatherDashboard" target="_blank"><
+                            button className="project-button">View Code</button></a>
+                </div>
+                <div className="project">
+                    <h3 className="project-title">Project 3</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                    https://github.com/josephwinston/workDayScheduler" target="_blank
+                    "><button className="project-button">View Code</button></a>
+                </div>
+                <div className="project">
+                    <h3 className="project-title">Project 4</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                    https://github.com/josephwinston/passwordGenerator" target="_blank"><
+                            button className="project-button">View Code</button></a>
+                </div>
+                <div className="project">
+                    <h3 className="project-title">Project 5</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                https://github.com/josephwinston/passwordGenerator" target="_blank"><
+                            button className="project-button">View Code</button></a>
+                </div>
+                <div className="project">
+                    <h3 className="project-title">Project 6</h3>
+                    <p className="project-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatum.</p>
+                    <a href="
+                    
+                    " target="_blank"><button className="project-button">View Code</button></a>
+                    </div>
+
+                </div>
+            </div>
+            );
 }
 
-export default Projects;
+            export default Projects;
