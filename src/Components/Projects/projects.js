6cbb2be16b0d1c8be520c490bfc7b1b802732723
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-26 19:40:19 +0530

    image position updated

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -7,13 +7,12 @@ const Projects = () => {
     return (
         <div className="main-content-area-box">
             <h2 className="headline">Projects</h2>
-            <div className="vertical" style={{ borderTop: "2px solid #fff ", marginLeft: 20, marginRight: 20 }}></div>
             <div className="project-box">
-                <div className="project-image kaam-dhaam">
-                    <img className="project1" src={Projectone} alt="project"/>
-                </div>
+                <img className="project1" src={Projectone} alt="project" />
                 <div className="project-discription">
-                    <p></p>
+                    <p>
+                        
+                    </p>
                 </div>
             </div>
         </div>
