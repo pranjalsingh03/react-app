d37d0a88468d1d67ea93dcccbcd7924f157a408e
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-27 20:04:33 +0530

    footer updated

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -6,7 +6,7 @@ const Footer = () => {
         <div>
         <footer className="bg-light text-center text-lg-start">
             <div className="text-center p-3" >
-            © 2021 Pranjal Singh:
+                Designed and Build By Pranjal Singh
             <a className="text-dark" href="www.google.com">
             </a>
             </div>
