d06128af988febb2da3699c91419de3acf42ae93
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-27 20:15:53 +0530

    class name updated

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -5,7 +5,7 @@ import './header.css';
 const Header = () => {
   return (
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <nav class="navbar navbar-expand-lg navbar-light">
       <div class="container-fluid">
         <a class="navbar-brand"
           href="https://github.com/pranjalsingh03">Pranjal Singh</a>
