be59f11fff96fe1a2bf9c53450cf0a4d82e31ff4
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-27 20:15:38 +0530

    backgroud changed

--- src/App.js
+++ src/App.js
@@ -11,9 +11,14 @@ import ContactUs from './Components/Contactus/contact';
 export default function MyApp() {
   return (
     <div>
+      <div id="home" className="intro route bg-image background">
+        <div id="stars" />
+        <div id="stars2" />
+        <div id="stars3" />
+        
       <Header />    {/*Header of the portfolio*/}
-      <Intro/>       {/* introduction  */}
-      <Home/>       {/*Home page */}
+      <Intro/>  </div>      {/* introduction  */}
+      <Home/>      {/*Home page */}
       <Projects/>    {/* Project page */}
       <ContactUs/>
       <Footer/>     {/*//Footer of the portfolio */}
