be60f6bbc947b0c9716e15526a5fafea8483d815
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-27 21:37:56 +0530

    project description

--- src/Components/Projects/projects.js
+++ src/Components/Projects/projects.js
@@ -12,16 +12,18 @@ const Projects = () => {
                 <div class="featured__StyledContent-sc-19vdtah-1 gWlhZI">
                     <h4 class="featured__StyledLabel-sc-19vdtah-2 dZTpBf">Featured Project</h4>
                     <h5 class="featured__StyledProjectName-sc-19vdtah-3 jRnWTV">
-                        <a href="www.link" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link">Face Mask Detection</a>
+                        <a href="www.link" target="_blank" rel="nofollow noopener noreferrer" aria-label="External Link">Web Calendar</a>
                     </h5>
                     <div class="featured__StyledDescription-sc-19vdtah-4 hwxmov">
                         <p>Face Mask Detection system to detect face masks in static
                             images as well as in real-time video streams. Own
                             custom dataset was built using Bing Search API and
                             Kaggle.</p>
-                        <p>Inspired by Adrian Rosebrock's
+
+                        {/* <p>Inspired by Adrian Rosebrock's
                             <a href="www.gothub.com" target="_blank" rel="nofollow noopener noreferrer">PyImageSearch Blog</a>
-                        </p>
+                        </p> */}
+                        
                     </div>
 
 
