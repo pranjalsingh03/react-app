e3bcaa6f08ff7ed14da23c46a215685e2f7c3e6a
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-06-30 14:00:31 +0530

    background

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./footer.css";
+import "../../App.css";
+
 
 const Footer = () => {
     return (
         <div>
-        <footer className="bg-light text-center text-lg-start">
+        <footer className="footer-class">
             <div className="text-center p-3" >
                 Designed and Build By Pranjal Singh
             <a className="text-dark" href="www.google.com">
