6d74f80c0fc1304d50685425255fa3c4a39387f3
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-07-07 09:15:05 +0530

    responsive

--- src/Components/Contactus/contact.js
+++ src/Components/Contactus/contact.js
@@ -1,18 +1,18 @@
 import React from "react";
 import "./contact.css"
 
-const ContactUs = () =>{
-     return(
+const ContactUs = () => {
+    return (
         <div className="main-contact-box">
-            
+
             <section id="contact">
-             <h6>What Next?</h6>
-            <h3 className="njaJNje">Get in Touch</h3> 
-            <div className="gUsVuV"><p>My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to mail me about any relevant job updates.</p></div>  
-            <a href="mailto:chandrikadeb7@gmail.com" target="_blank" rel="nofollow noopener noreferrer" class="contact__StyledEmailLink-sc-sehid-3 dXyrwW">Mail Me</a>
+                <h6>What Next?</h6>
+                <h3 className="njaJNje">Get in Touch</h3>
+                <div className="gUsVuV"><p>My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to mail me about any relevant job updates.</p></div>
+                <a href="mailto:chandrikadeb7@gmail.com" target="_blank" rel="nofollow noopener noreferrer" class="contact__StyledEmailLink-sc-sehid-3 dXyrwW">Mail Me</a>
             </section>
         </div>
-     );
+    );
 }
 
 export default ContactUs;
