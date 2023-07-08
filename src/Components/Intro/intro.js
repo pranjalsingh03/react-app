0c24be31c710880756d06ee28604771b557a49a7
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-07-08 09:16:20 +0530

    formate document

--- src/Components/Intro/intro.js
+++ src/Components/Intro/intro.js
@@ -10,50 +10,50 @@ const Intro = () => {
     return (
 
         <div className="main-box">
-        
-        {/* left side of the div */}
+
+            {/* left side of the div */}
             <div className="main-box-content-left">
                 <span className="first-name">Pranjal Singh</span>
                 <div className="about-user">Brings ideas to life with code.
-                <div className="jsUDeb">
-                <a href="https://github.com/pranjalsingh03">
-                        <img src={github} alt="github" className="responsive-logo-intro"/>
+                    <div className="jsUDeb">
+                        <a href="https://github.com/pranjalsingh03">
+                            <img src={github} alt="github" className="responsive-logo-intro" />
                         </a>
                         <a href="https://www.linkedin.com/in/pranjalsingh03/">
-                        <img src={linkedin} alt="linkedin" className="responsive-logo-intro" />
+                            <img src={linkedin} alt="linkedin" className="responsive-logo-intro" />
                         </a>
                         <a href="https://www.instagram.com/sanatani_magan09/">
-                        <img src={twitter} alt="twiter" className="responsive-logo-intro" />
+                            <img src={twitter} alt="twiter" className="responsive-logo-intro" />
                         </a>
-                </div>
+                    </div>
                 </div>
                 <div orientation="left"
                     class="side__StyledContainer-sc-18y99l7-0 gXnnBS">
-                    <div 
-                    class="email__StyledLinkWrapper-sc-1fu3fru-0 dvseSq fade-enter-done">
+                    <div
+                        class="email__StyledLinkWrapper-sc-1fu3fru-0 dvseSq fade-enter-done">
                         <a href="https://github.com/pranjalsingh03">
-                        <img src={github} alt="github" className="Image-logo imagelogo"/>
+                            <img src={github} alt="github" className="Image-logo imagelogo" />
                         </a>
                         <a href="https://www.linkedin.com/in/pranjalsingh03/">
-                        <img src={linkedin} alt="linkedin" className="Image-logo imagelogo" />
+                            <img src={linkedin} alt="linkedin" className="Image-logo imagelogo" />
                         </a>
                         <a href="https://www.instagram.com/sanatani_magan09/">
-                        <img src={instagram} alt="instagram" className="Image-logo imagelogo"/>
+                            <img src={instagram} alt="instagram" className="Image-logo imagelogo" />
                         </a>
                         <a href="https://www.instagram.com/sanatani_magan09/">
-                        <img src={twitter} alt="twiter" className="Image-logo imagelogo" />
+                            <img src={twitter} alt="twiter" className="Image-logo imagelogo" />
                         </a>
                         <a href="https://www.facebook.com/magan728">
-                        <img src={facebook} alt="facebook" className="facebook-logo imagelogo" />
+                            <img src={facebook} alt="facebook" className="facebook-logo imagelogo" />
                         </a>
                     </div>
                 </div>
             </div>
             {/* Right side of the div */}
-                
-                <div orientation="right" class="side__StyledContainer-sc-18y99l7-0 lbtSgG"><div class="email__StyledLinkWrapper-sc-1fu3fru-0 dvseSq fade-enter-done"><a href="mailto:pranjalmagansingh9304@gmail.com" class="email__StyledEmailLink-sc-1fu3fru-1 gduzAh">pranjalmagansingh9304@gmail.com</a></div></div>
-            </div>
-            // </div>
+
+            <div orientation="right" class="side__StyledContainer-sc-18y99l7-0 lbtSgG"><div class="email__StyledLinkWrapper-sc-1fu3fru-0 dvseSq fade-enter-done"><a href="mailto:pranjalmagansingh9304@gmail.com" class="email__StyledEmailLink-sc-1fu3fru-1 gduzAh">pranjalmagansingh9304@gmail.com</a></div></div>
+        </div>
+        // </div>
     )
 }
 
