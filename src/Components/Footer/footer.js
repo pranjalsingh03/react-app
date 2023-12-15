ff7882807b0369fa9ff5e6690179a89dfd7bb9fb
Pranjal-singh <pranjalsingh9304@gmail.com>
2023-12-15 22:10:33 +0530

    Refactor footer component styles and add social media links

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -9,16 +9,17 @@ import gh from "../../Images/github.svg"
 const Footer = () => {
     return (
         <footer className="text-center">
-        <div className=" text-muted py-4 py-lg-5">
+        <div>
             <ul className="list-inline">
                 <li className="list-inline-item me-4"><a className="link-secondary" href="/">Web design</a></li>
                 <li className="list-inline-item me-4"><a className="link-secondary" href="/">Development</a></li>
                 <li className="list-inline-item"><a className="link-secondary" href="/">Hosting</a></li>
             </ul>
             <ul className="list-inline">
-                <li className="list-inline-item me-4"> <img classNameName="image-foot" src={ld} alt="facebook" /></li>
-                <li className="list-inline-item me-4"><img classNameName="image-foot" src={tw} alt="" /></li>
-                <li className="list-inline-item"><img className="image-foot" src={gh} alt="" /></li>
+                <li className="list-inline-item me-4"> <a href="https://www.linkedin.com/in/pranjalsingh03/"><img className="image-foot" src={ld} alt="linkedin" /></a></li>
+                <li className="list-inline-item me-4"><a href="/"><img className="image-foot" src={tw} alt="" /></a></li>
+                        
+                <li className="list-inline-item"><a href="https://github.com/pranjalsingh03"><img className="image-foot" src={gh} alt="" /></a></li>
             </ul>
             <p className="text-center">Design and Build by Pranjal singh</p>
         </div>
