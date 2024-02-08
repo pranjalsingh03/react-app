3dcb84a12a68805fd418ee7b6181f58f6686350e
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-08 20:07:29 +0530

    Update contact email address

--- src/Components/Contactus/contact.js
+++ src/Components/Contactus/contact.js
@@ -8,8 +8,8 @@ const ContactUs = () => {
             <section id="contact">
                 <h6 className="cont-head">What Next?</h6>
                 <h3 className="njaJNje">Get in Touch</h3>
-                <div className="gUsVuV"><p>My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to mail me about any relevant job updates.</p></div>
-                <a href="mailto:singhmagan9304@gmail.com" target="_blank" rel="nofollow noopener noreferrer" class="contact__StyledEmailLink-sc-sehid-3 dXyrwW">Mail Me</a>
+                <div className="gUsVuV"><p>My inbox is always open. Whether you have a question or just want to say hello, I'll try my best to get back to you! Feel free to mail me about any relevant updates.</p></div>
+                <a href="mailto:pranjalmagansingh9304@gmail.com" target="_blank" rel="nofollow noopener noreferrer" class="contact__StyledEmailLink-sc-sehid-3 dXyrwW">Mail Me</a>
             </section>
         </div>
     );
