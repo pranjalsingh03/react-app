63ca5ca095c371b6669469ff15545fa17aad0e49
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-16 23:16:12 +0530

    Update Lang.css and Lang.js

--- src/Components/Languages/Lang.js
+++ src/Components/Languages/Lang.js
@@ -1,17 +1,34 @@
 import React from 'react'
 import './Lang.css';
 
+
 export default function Lang() {
+  function Technical(){
+    return(
+      <>
+      <div className="container">
+      <h1 className="text-center">Technical Skills</h1>
+      </div>
+      </>
+    )
+  }
+
   return (
     <>
     <div className="container">
-      <h1 className="skills text-center">
-        Skills
-      </h1>
-      <ul className='tech-non'>
-        <li>Technical skills</li>
-      </ul>
+      <h1 className="text-center">Skills</h1>
+  <div className="row">
+    <div className="col-sm text-center border-solid" onClick={Technical}>
+      Technical Skills
     </div>
+    <div className="col-sm text-center border-solid">
+      Soft Skills
+    </div>
+  </div>
+  <section>
+    he;;p
+  </section>
+</div>
     </>
   )
 }
