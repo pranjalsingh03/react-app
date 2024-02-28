7b9333a6ccf8c34449991c3e2b805d873fe6a1ee
Pranjal-singh <pranjalsingh9304@gmail.com>
2024-02-28 22:37:40 +0530

    Hide contact us button and comment out navbar toggler

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -38,13 +38,13 @@ const Header = () => {
           <Moon /><Sun />
         </label>
       </div>
-      <button className="navbar-toggler"
+      {/* <button className="navbar-toggler"
         type="button" data-bs-toggle="collapse"
         data-bs-target="#navbarNavAltMarkup"
         aria-controls="navbarNavAltMarkup"
         aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
-      </button>
+      </button> */}
       <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
         {/* <ul className="navbar-nav ms-auto ">
           <li className="nav-item">
